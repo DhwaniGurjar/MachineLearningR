2060757aeca47bce38dd1b23905c125ec0dd1f6e
Dhwani <dhwanigurjar11@gmail.com>
2020-04-28 23:23:06 -0400

    Simple Linear Regression

---
+++ Simple Linear Regression/SimpleLinearRegression.R
@@ -0,0 +1,30 @@
+# Simple Linear Regression
+
+# Importing the dataset
+dataset = read.csv('Salary_Data.csv')
+
+# Splitting dataset into Training set and Test set
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Salary, SplitRatio = 2/3)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Fitting Simple Linear Regression to the Training Set
+regressor = lm(formula = Salary ~ YearsExperience, data = training_set)
+
+# Predict the Test set results
+y_pred = predict(regressor, newdata = test_set)
+
+#Visualising the Training set results
+#install.packages('ggplot2')
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = training_set$YearsExperience, y = training_set$Salary),
+             colour = 'red') +
+  geom_line(aes(x = training_set$YearsExperience, y = predict(regressor,
+                      newdata = training_set)),
+            colour = 'blue') +
+  ggtitle('Salary vs Experience (Training Set)') +
+  xlab('Years of Experience') + 
+  ylab('Salary')
