393ae5b13e7d50b47be4224d868ac5d0f67a5f68
Dhwani <dhwanigurjar11@gmail.com>
2020-05-01 17:57:46 -0400

    Renamed folders

--- 2. Simple Linear Regression/SimpleLinearRegression.R
+++ 2. Simple Linear Regression/SimpleLinearRegression.R
@@ -28,3 +28,16 @@ ggplot() +
   ggtitle('Salary vs Experience (Training Set)') +
   xlab('Years of Experience') + 
   ylab('Salary')
+
+
+#Visualising the Test set results
+library(ggplot2)
+ggplot()+
+  geom_point(aes(x = test_set$YearsExperience, y = test_set$Salary),
+             colour = 'red') +
+  geom_line(aes(x = training_set$YearsExperience, y = predict(regressor,
+                newdata = training_set)), 
+             colour = 'blue') +
+  ggtitle('Salary vs Experience (Test set)') +
+  xlab('Years of Experience') +
+  ylab('Salary')
